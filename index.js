1983ead570ea5b3869660855c6c431fe0ccc358f
Kevin Kwok <antimatter15@gmail.com>
2017-03-15 19:32:25 -0400

    demo

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-function PinchZoom(selection){
-    var zoom = selection.property("__zoom");
-    if(!zoom) throw "selection.call(zoom) should happen before .call(PinchZoom)";
+function PinchZoom(selection, zoom){
+    if(!zoom) throw "zoom must be passed as a second argument";
 
     var isMac = navigator.platform.toUpperCase().indexOf('MAC')>=0;
     if(!isMac) return;
@@ -11,27 +10,34 @@ function PinchZoom(selection){
         .on('gesturestart', gesturestart)
         .on('gestureend', gestured);
 
+    function wheeled(){
+        var e = d3.event;
+        e.preventDefault()
+        
+        if(e.metaKey || e.ctrlKey){
+            // on Chrome, pinch-zoom maps to Ctrl+Scroll
+            zoom.scaleBy(selection, Math.pow(0.98, e.deltaY))
+        } else {
+            // Scroll to pan
+            var scale = d3.zoomTransform(selection.node()).k;
+            zoom.translateBy(selection, -e.deltaX / scale, -e.deltaY / scale)
+        }
+    }
 
     var initialScale = 1;
+    function gesturestart(){
+        // Safari uses their weird gesturestart/gestureend to zoom trick
+        initialScale = d3.zoomTransform(selection.node()).k;
+        d3.event.preventDefault()
+    }
+
     function gestured(){
         var e = d3.event;
         zoom.scaleTo(selection, e.scale * initialScale)
         e.preventDefault()
     }
+}
 
-    function gesturestart(){
-        initialScale = d3.zoomTransform(selection.node()).k;
-        d3.event.preventDefault()
-    }
-
-    function wheeled(){
-        var e = d3.event;
-        e.preventDefault()
-        if(e.metaKey || e.ctrlKey){
-            zoom.scaleBy(selection, Math.pow(0.98, e.deltaY))
-        } else {
-            var scale = d3.zoomTransform(selection.node()).k;
-            zoom.translateBy(selection, -e.deltaX / scale, -e.deltaY / scale)
-        }
-    }
+if(typeof exports === "object" && exports) {  
+    module.exports = PinchZoom;
 }
